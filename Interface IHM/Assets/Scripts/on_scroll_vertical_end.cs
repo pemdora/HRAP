ca4d6237b3da4315c52ec25d6c021e11850ead74
badre <badre.fo@gmail.com>
2017-10-18 21:20:22 +0200

    UI conception
    5 Scenes = Menu principal + Terms of contract + Identification + Entretien + Rmerciements

---
+++ Interface IHM/Assets/Scripts/on_scroll_vertical_end.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class on_scroll_vertical_end : MonoBehaviour {
+	public  Scrollbar vertical_scrollbar;
+	public  Button button_continue;
+
+	// Use this for initialization
+	void Start () {
+
+		Set_Interactable_Button (false); // rends indisponible le bouton 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (vertical_scrollbar.value == 0) // tant que le scroll n'est pas en bas
+		{
+			Set_Interactable_Button (true);// rendre le bouton clicable 
+		}
+	}
+
+	void Set_Interactable_Button(bool b)
+	{
+		button_continue.interactable = b;
+	}
+}
