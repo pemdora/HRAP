693c491004ed0c7a4459dc177875cdff02691f15
badre <badre.fo@gmail.com>
2017-10-21 17:35:16 +0200

    Project name changed + first IHM
    5 scenes

---
+++ HRAP/Assets/Scripts/Input_authentification.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+//Ce code permettra de traiter les inputs, de les faire valider via la BDD
+public class Input_authentification : MonoBehaviour {
+
+	public InputField firstName;
+	public InputField lastName;
+	public Button continue_b;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
