31974330602857feb02da36e3e35a28b2f906428
tarazhong <zhong.tara@gmail.com>
2017-11-23 10:59:03 +0100

    Modify Interview scene
    Change landscape for interview scene, add temporary candidate and AI.

---
+++ Decor/Assets/ArchVizPRO Interior Vol.5/3D SCRIPT/Billboard.js
@@ -0,0 +1,13 @@
+﻿
+
+#pragma strict
+ var target : Transform;
+ 
+ function Start () {
+ target = GameObject.Find("Target_Billboard").transform;
+
+var targetPostition : Vector3  = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+ transform.LookAt(targetPostition);
+ transform.Rotate(0, 180, 0);
+ }
+
