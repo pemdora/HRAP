be59087a1536fbcec2a24de109b75afb7c87ef66
tarazhong <zhong.tara@gmail.com>
2017-11-24 22:48:10 +0100

    Replace AI movement
    Replace manual movments for AI with Navmesh agent from Unity

--- Assets/Scripts/AImovment.cs
+++ Assets/Scripts/AImovment.cs
@@ -1,38 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class AImovment : MonoBehaviour
 {
-    private float speed = 1f;
-    private Vector3 direction;
-    public Transform directionCandidate;
+    public Transform directionCandidate; // waypoint 1
     private Animator animator;
+    private NavMeshAgent agent;
 
-
-    void Awake()
+    void Start()
     {
         // Find a reference to the Animator component in Awake since it exists in the scene.
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
         
     }
-
-    public void LookAt(Vector3 positionToLookAt)
+    
+    public void MoveTopoint(Vector3 point)
     {
-        this.direction = positionToLookAt - this.transform.position;
-        this.direction.y = 0;
-
-        if (this.direction != Vector3.zero)
-        {
-            Quaternion lookRotation = Quaternion.LookRotation(direction); // mathematical way to deal with rotation
-            Vector3 rotation = Quaternion.Lerp(this.transform.rotation, lookRotation, Time.deltaTime * 10f).eulerAngles;
-            this.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f); // will just aim in the (x,z) plan
-        }
-    }
-
-    public void Move()
-    {
-        this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        agent.SetDestination(point);
     }
 
     // Update is called once per frame
@@ -40,8 +28,7 @@ public class AImovment : MonoBehaviour
     {
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walking"))
         {
-            LookAt(directionCandidate.position);
-            Move();
+            MoveTopoint(directionCandidate.position);
         }
     }
     
