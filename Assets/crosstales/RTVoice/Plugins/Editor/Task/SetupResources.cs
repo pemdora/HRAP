863faba9b2ae85ba9d72659f75e8fa3017da32ec
tarazhong <zhong.tara@gmail.com>
2017-12-08 14:45:09 +0100

    Add Asset RT Pro for voice

---
+++ Assets/crosstales/RTVoice/Plugins/Editor/Task/SetupResources.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Crosstales.RTVoice.EditorTask
+{
+    /// <summary>Moves all needed resources to 'Editor Default Resources'.</summary>
+    [InitializeOnLoad]
+    public static class SetupResources
+    {
+
+        #region Constructor
+
+        static SetupResources()
+        {
+
+#if !rtv_ignore_setup
+
+            string path = Application.dataPath + "/";
+            string assetpath = "Assets/crosstales/RTVoice/";
+
+            string sourceFolder = path + "crosstales/RTVoice/Icons/";
+            string source = assetpath + "Icons/";
+
+            string targetFolder = path + "Editor Default Resources/RTVoice/";
+            string target = "Assets/Editor Default Resources/RTVoice/";
+            string metafile = assetpath + "Icons.meta";
+
+            try
+            {
+                if (System.IO.Directory.Exists(sourceFolder))
+                {
+                    if (!System.IO.Directory.Exists(targetFolder))
+                    {
+                        System.IO.Directory.CreateDirectory(targetFolder);
+                    }
+
+                    var dirSource = new System.IO.DirectoryInfo(sourceFolder);
+
+                    foreach (var file in dirSource.GetFiles("*"))
+                    {
+                        AssetDatabase.MoveAsset(source + file.Name, target + file.Name);
+
+                        if (Util.Config.DEBUG)
+                            Debug.Log("File moved: " + file);
+                    }
+
+                    dirSource.Delete();
+
+                    if (System.IO.File.Exists(metafile))
+                    {
+                        System.IO.File.Delete(metafile);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError("Could not move all files: " + ex);
+            }
+            finally
+            {
+                AssetDatabase.Refresh();
+            }
+#endif
+        }
+
+        #endregion
+    }
+}
+// © 2016-2017 crosstales LLC (https://www.crosstales.com)
