de0ef0a100944f6c9233c763913d7d6b0ddcb486
tarazhong <zhong.tara@gmail.com>
2017-12-11 17:50:24 +0100

    Create script for speech manager

---
+++ Assets/Scripts/Animation/SpeechManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RogoDigital.Lipsync;
+
+public class SpeechManager : MonoBehaviour {
+
+    public LipSync lipsyncComponent;
+    public LipSyncData clip1;
+    public LipSyncData clip2;
+    int i = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        lipsyncComponent.Play(clip1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
