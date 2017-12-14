d1e3504a061dbe81006d7c8a354b537a15fcdee7
tarazhong <zhong.tara@gmail.com>
2017-12-14 15:47:02 +0100

    Update CameraManager.cs

--- Assets/Scripts/Animation/CameraManager.cs
+++ Assets/Scripts/Animation/CameraManager.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HRAP;
 
 public class CameraManager : MonoBehaviour
 {
     public Camera camera_PE_1; // Plan d'ensemble
     public Camera camera_PA_1; // Plan Americain avec 1 seul personnage
     public Camera camera_PA_2; // Plan Americain avec les 2 personnages
+    public Camera camera_PR_1; // Plan rapproché 1
+    public Camera camera_PR_2; // Plan rapproché 2
+    public Camera camera_PR_3; // Plan rapproché 3
+    public Camera camera_PR_4; // Plan rapproché 4
+    public Camera camera_PR_5; // Plan rapproché 5
     public Camera camera_GP_1; // Gros Plan
 
     // Use this for initialization
@@ -16,17 +22,125 @@ public class CameraManager : MonoBehaviour
         camera_PA_1.enabled = false;
         camera_PA_2.enabled = false;
         camera_GP_1.enabled = false;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
 
-        if (Input.GetKeyDown(KeyCode.C))
+    private void Display(M_Camera camera,bool displayIHM) // Display current camera
+    {
+        switch (camera)
         {
-            camera_PE_1.enabled = !camera_PE_1.enabled;
-            camera_GP_1.enabled = !camera_GP_1.enabled;
+            case M_Camera.GP_1:
+                camera_GP_1.enabled = true;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PA_1:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = true;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PA_2:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = true;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PE_1:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = true;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PR_1:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = true;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PR_2:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = true;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PR_3:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = true;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PR_4:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = true;
+                camera_PR_5.enabled = false;
+                break;
+            case M_Camera.PR_5:
+                camera_GP_1.enabled = false;
+                camera_PA_1.enabled = false;
+                camera_PA_2.enabled = false;
+                camera_PE_1.enabled = false;
+                camera_PR_1.enabled = false;
+                camera_PR_2.enabled = false;
+                camera_PR_3.enabled = false;
+                camera_PR_4.enabled = false;
+                camera_PR_5.enabled = true;
+                break;
         }
+        if (displayIHM)
+        {
+            IHMInterview.MaskAllNguiComponents(true);
+        }
+        else
+        {
+            IHMInterview.MaskAllNguiComponents(false);
+        }
+
     }
 }
