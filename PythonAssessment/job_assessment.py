f36c82418fce6c660af3dd93b45de0f08b89fe7d
tarazhong <zhong.tara@gmail.com>
2018-01-11 10:04:51 +0100

    Add --predict and --accuracy option, now we can predict a profile for the whole train set

--- PythonAssessment/job_assessment.py
+++ PythonAssessment/job_assessment.py
@@ -20,14 +20,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
+# Limit samples has to be fixed to not ork with predict
 
-# Setup logging
-logger = logging.getLogger('job_assessment.py')
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-logger.addHandler(ch)
+''' Predict for a given profile '''
+def train_predict(classifier_name, classifier,X_train, y_train) :
+    # Do Training@
+    t0 = time.time()
+    classifier_result=classifier.fit(X_train, y_train)
+    logger.info("Training done in %0.3fs" % (time.time() - t0))
 
+
+    print("Prediction for the given profile")
+    print(classifier.predict([[6, 7, 6, 8, 7, 8, 6, 6, 7]])) # predict a given example
+
+''' Report Classifier accuracy '''
 # Train and print metrics for a given classifier with training set and test test
 # Return train accuracy score and test accuracy score
 def classifier_train_test(classifier_name, classifier,X_train, X_test, y_train, y_test) :
@@ -48,10 +54,9 @@ def classifier_train_test(classifier_name, classifier,X_train, X_test, y_train, 
     print('Score on testing : %f' % classifier.score(X_test, y_test))
     print(metrics.classification_report(y_test,predicted_test))
 
-    print(classifier.predict([[6, 7, 6, 8, 7, 8, 6, 6, 7]])) # predict a given example
-
     return classifier.score(X_train, y_train),metrics.accuracy_score(y_test,predicted_test)
 
+''' Report Learning impact on accuracy '''
 # Split the initial train set into 10 test set (1%,10%,20%,40%,60%,80%,100%) of original training set
 def learning_impact(classifier_name,classifier,X_train,y_train,X_test,y_test):
     samples_train_rate=[0.01,0.10,0.20,0.40,0.60,0.80,1.00]
@@ -83,6 +88,8 @@ def display_learning_curve(title,samples_train,train_accuracy,test_accuracy) :
     plt.legend(loc="best")
     return plt
 
+''' Report Testing impact on accuracy '''
+# Report Testig impact on accuracy
 # Split the initial test set into 10 test set (1%,10%,20%,40%,60%,80%,99%) of original testing set
 def testing_impact(classifier_name,classifier,X_train,y_train,X_test,y_test):
     samples_test_rate=[0.01,0.10,0.20,0.40,0.60,0.80,0.99]
@@ -123,17 +130,29 @@ def display_testing_curve(title,samples_test,mean_accuracy,std_mean_accuracy) :
     plt.legend(loc="best")
     return plt
 
+
+
+# Setup logging
+logger = logging.getLogger('job_assessment.py')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(ch)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract features, train a classifier on images and test the classifier')
     input_group = parser.add_mutually_exclusive_group(required=True)
     input_group.add_argument('--csv-file',help='file containing the people profile, one per line, comma separated')
     # input_group.add_argument('--load-features',help='read features and class from pickle file')
     # parser.add_argument('--save-features',help='save features in pickle format')
-    parser.add_argument('--limit-samples',type=int, help='limit the number of samples to consider for training')
-    classifier_group = parser.add_mutually_exclusive_group(required=True)
+    result_group =  parser.add_mutually_exclusive_group(required=True)
+    result_group.add_argument('--predict',action='store_true')
+    result_group.add_argument('--accuracy',action='store_true')
+    classifier_group = parser.add_mutually_exclusive_group(required=False)
     classifier_group.add_argument('--nearest-neighbors',type=int)
     classifier_group.add_argument('--nearest-neighbors-logistic-regression', action='store_true', help='train knn with 60% training, 20% validation select the best k for k=1 to k=9, 20% test then do logistic regression')
     classifier_group.add_argument('--logistic-regression', action='store_true')
+    parser.add_argument('--limit-samples',type=int, help='limit the number of samples to consider for training')
     # classifier_group.add_argument('--features-only', action='store_true', help='only extract features, do not train classifiers')
     learning_testing_impact =  parser.add_mutually_exclusive_group(required=False)
     learning_testing_impact.add_argument('--learning-curve',action='store_true',help='study the impact of a growing training set [(1%,10%,20%,40%,60%,80%,100%) of original training set] on the accuracy')
@@ -184,49 +203,68 @@ if __name__ == "__main__":
         logger.info('No classifier to train, exit')
         sys.exit()
     '''
-
     if args.nearest_neighbors:
-        # create KNN classifier with args.nearest_neighbors as a parameter
-        neigh = neighbors.KNeighborsClassifier(n_neighbors=args.nearest_neighbors)
-        logger.info('Use kNN classifier with k= {}'.format(args.nearest_neighbors))
+        # Predict for a given profil
+        if args.predict:
+            # create KNN classifier with args.nearest_neighbors as a parameter
+            neigh = neighbors.KNeighborsClassifier(n_neighbors=args.nearest_neighbors)
+            logger.info('Use kNN classifier with k= {}'.format(args.nearest_neighbors))
+            logger.info("Train set size is {}".format(X.shape))
 
-        # Use train_test_split to create train and validation/test split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-        logger.info("Train set size is {}".format(X_train.shape))
-        logger.info("Test set size is {}".format(X_test.shape))
-
-        # If we want to study Increasing training set impact
-        if args.learning_curve:
-            learning_impact("KNN",neigh,X_train,y_train,X_test,y_test);
-        elif args.testing_curve:
-            testing_impact("KNN",neigh,X_train,y_train,X_test,y_test)
+            train_predict("KNN",neigh,X,y);
+        # Print classifier accuracy
         else:
-            # Do Training and testing
-            train_accuracy, test_accuracy = classifier_train_test("KNN",neigh,X_train, X_test, y_train, y_test)
-            print('Knn Train accuracy score : ',train_accuracy)
-            print('Knn Test accuracy score :', test_accuracy)
+            # create KNN classifier with args.nearest_neighbors as a parameter
+            neigh = neighbors.KNeighborsClassifier(n_neighbors=args.nearest_neighbors)
+            logger.info('Use kNN classifier with k= {}'.format(args.nearest_neighbors))
+
+            # Use train_test_split to create train and validation/test split
+            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+            logger.info("Train set size is {}".format(X_train.shape))
+            logger.info("Test set size is {}".format(X_test.shape))
+
+            # If we want to study Increasing training set impact
+            if args.learning_curve:
+                learning_impact("KNN",neigh,X_train,y_train,X_test,y_test);
+            elif args.testing_curve:
+                testing_impact("KNN",neigh,X_train,y_train,X_test,y_test)
+            else:
+                # Do Training and testing
+                train_accuracy, test_accuracy = classifier_train_test("KNN",neigh,X_train, X_test, y_train, y_test)
+                print('Knn Train accuracy score : ',train_accuracy)
+                print('Knn Test accuracy score :', test_accuracy)
 
 
     elif args.logistic_regression:
-        # create logistic regression classifier with args.logistic_regression as a parameter
-        logistic_reg = LogisticRegression()
-        logger.info('Use logistic_regression classifier with k= {}'.format(args.logistic_regression))
+        # Predict for a given profil
+        if args.predict:
+            # create KNN classifier with args.nearest_neighbors as a parameter
+            logistic_reg = LogisticRegression()
+            logger.info('Use logistic_regression classifier')
+            logger.info("Train set size is {}".format(X.shape))
 
-        # Use train_test_split to create train/test split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-        logger.info("Train set size is {}".format(X_train.shape))
-        logger.info("Test set size is {}".format(X_test.shape))
-
-        # If we want to study Increasing training set impact
-        if args.learning_curve:
-            learning_impact("Logistic Regression",logistic_reg,X_train,y_train,X_test,y_test);
-        elif args.testing_curve:
-            testing_impact("Logistic Regression",logistic_reg,X_train,y_train,X_test,y_test)
+            train_predict("Logistic Regression",logistic_reg,X,y);
+        # Print classifier accuracy
         else:
-            # Do Training and testing
-            train_accuracy, test_accuracy = classifier_train_test("Logistic Regression",logistic_reg,X_train, X_test, y_train, y_test)
-            print('Logistic Train accuracy score :', train_accuracy)
-            print('Logistic Test accuracy score :', test_accuracy)
+            # create logistic regression classifier
+            logistic_reg = LogisticRegression()
+            logger.info('Use logistic_regression classifier')
+
+            # Use train_test_split to create train/test split
+            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+            logger.info("Train set size is {}".format(X_train.shape))
+            logger.info("Test set size is {}".format(X_test.shape))
+
+            # If we want to study Increasing training set impact
+            if args.learning_curve:
+                learning_impact("Logistic Regression",logistic_reg,X_train,y_train,X_test,y_test);
+            elif args.testing_curve:
+                testing_impact("Logistic Regression",logistic_reg,X_train,y_train,X_test,y_test)
+            else:
+                # Do Training and testing
+                train_accuracy, test_accuracy = classifier_train_test("Logistic Regression",logistic_reg,X_train, X_test, y_train, y_test)
+                print('Logistic Train accuracy score :', train_accuracy)
+                print('Logistic Test accuracy score :', test_accuracy)
 
 
     elif args.nearest_neighbors_logistic_regression:
@@ -324,4 +362,3 @@ if __name__ == "__main__":
     else:
         logger.error('No classifier specified')
         sys.exit()
-
