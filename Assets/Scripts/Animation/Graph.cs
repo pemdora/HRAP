9f0dd2dd831cce9785807222ebcee993001d6fe1
tarazhong <zhong.tara@gmail.com>
2018-01-16 09:27:44 +0100

    Complete graph

--- Assets/Scripts/Animation/Graph.cs
+++ Assets/Scripts/Animation/Graph.cs
@@ -14,9 +14,25 @@ public class Graph : MonoBehaviour
     // Axis Values
     private int nbQualities;// Number of qualities
     Dictionary<int, List<Transform>> axis = new Dictionary<int, List<Transform>>(); // Dictionnary of each axis with points
-    public float[] radianList;
+    private float[] radianList;
     public int[] qualities = new int[9];
     private Transform[] qualitiesPoints = new Transform[9];
+
+
+    // Material used for the connecting lines
+    public Material lineMat;
+
+    // Connect all of the `points` to the `mainPoint`
+    // public GameObject mainPoint;
+    // public GameObject[] points;
+
+    // Fill in this with the default Unity Cylinder mesh
+    // We will account for the cylinder pivot/origin being in the middle.
+    public Mesh cylinderMesh;
+
+
+    GameObject[] ringGameObjects;
+
     void Awake()
     {
         radianList = new float[] { 0f, 0.689f, 1.396f, 2.094f, 2.792f, 3.490f, 4.188f, 4.886f, 5.585f };
@@ -58,85 +74,71 @@ public class Graph : MonoBehaviour
             }
         }
 
-        /*
-        // Pour tracer les droites 
-        // un peu de maths de seconde
-        float m = 0; // coef directeur
-        float p = 0; // ordonnée à l'origine
-        float y = 0; // plus petit ordonnée entre les 2 points
-        float resolution = 0;
-        float num = 0, denum = 0;
-        for (int i = 0; i < nbQualities -1; i++)
-        {
-            // trouver le coef directeur de la droite entre 2 points
-            m = (qualitiesPoints[i].position.y - qualitiesPoints[i + 1].position.y) / (qualitiesPoints[i].position.x - qualitiesPoints[i + 1].position.x);
-            p = qualitiesPoints[i].position.y - m * qualitiesPoints[i].position.x;
-
-            y = 0;
-            if(qualitiesPoints[i].position.y< qualitiesPoints[i + 1].position.y)
-            {
-                y = qualitiesPoints[i].position.y;
-            }
-            else
-            {
-                y = qualitiesPoints[i+1].position.y;
-            }
-            
-            if ((Mathf.Abs(qualitiesPoints[i].position.y) + Mathf.Abs(qualitiesPoints[i + 1].position.y))> (Mathf.Abs(qualitiesPoints[i].position.x) + Mathf.Abs(qualitiesPoints[i + 1].position.x)))
-            {
-                num = Mathf.Abs(qualitiesPoints[i].position.y) + Mathf.Abs(qualitiesPoints[i + 1].position.y);
-                denum = Mathf.Abs(qualitiesPoints[i].position.x) + Mathf.Abs(qualitiesPoints[i + 1].position.x);
-            }
-            else
-            {
-                denum = Mathf.Abs(qualitiesPoints[i].position.y) + Mathf.Abs(qualitiesPoints[i + 1].position.y);
-                num = Mathf.Abs(qualitiesPoints[i].position.x) + Mathf.Abs(qualitiesPoints[i + 1].position.x);
-            }
-
-            resolution = num / denum;
-            Debug.Log(resolution);
-            for (int j = 0; j < 100; j++)
-            {
-                point = Instantiate(pointPrefabAxis);
-                position.y = y + j*0.01f;
-                position.x = (position.y - p) / m;
-                point.localPosition = position;
-                points.Add(point);
-            
-        }
-
-        // Tracer la derniere droite
-        m = (qualitiesPoints[nbQualities-1].position.y - qualitiesPoints[0].position.y) / (qualitiesPoints[nbQualities - 1].position.x - qualitiesPoints[0].position.x);
-        p = qualitiesPoints[nbQualities - 1].position.y - m * qualitiesPoints[0].position.x;
-        if (qualitiesPoints[nbQualities - 1].position.y < qualitiesPoints[0].position.y)
-        {
-            y = qualitiesPoints[nbQualities - 1].localPosition.y;
-        }
-        else
-        {
-            y = qualitiesPoints[0].localPosition.y;
-        }
-        for (int j = 0; j < 50; j++)
-        {
-            point = Instantiate(pointPrefabAxis);
-            position.y = y + j * radius;
-            position.x = (position.y - p) / m;
-            point.localPosition = position;
-            points.Add(point);
-        }*/
-        
-
     }
 
     // Use this for initialization
     void Start()
     {
+        this.ringGameObjects = new GameObject[qualitiesPoints.Length];
+        //this.connectingRings = new ProceduralRing[points.Length];
+        for (int i = 0; i < qualitiesPoints.Length; i++)
+        {
+            // Make a gameobject that we will put the ring on
+            // And then put it as a child on the gameobject that has this Command and Control script
+            this.ringGameObjects[i] = new GameObject();
+            this.ringGameObjects[i].name = "Connecting ring #" + i;
+            this.ringGameObjects[i].transform.parent = this.gameObject.transform;
 
+            // We make a offset gameobject to counteract the default cylindermesh pivot/origin being in the middle
+            GameObject ringOffsetCylinderMeshObject = new GameObject();
+            ringOffsetCylinderMeshObject.transform.parent = this.ringGameObjects[i].transform;
+
+            // Offset the cylinder so that the pivot/origin is at the bottom in relation to the outer ring gameobject.
+            ringOffsetCylinderMeshObject.transform.localPosition = new Vector3(0f, 1f, 0f);
+            // Set the radius
+            ringOffsetCylinderMeshObject.transform.localScale = new Vector3(radius, 1f, radius);
+
+            // Create the the Mesh and renderer to show the connecting ring
+            MeshFilter ringMesh = ringOffsetCylinderMeshObject.AddComponent<MeshFilter>();
+            ringMesh.mesh = this.cylinderMesh;
+
+            MeshRenderer ringRenderer = ringOffsetCylinderMeshObject.AddComponent<MeshRenderer>();
+            ringRenderer.material = lineMat;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float cylinderDistance = 0;
+        for (int i = 0; i < nbQualities - 1; i++)
+        {
+            // Move the ring to the point
+            this.ringGameObjects[i].transform.position = this.qualitiesPoints[i].transform.position;
 
+            // Match the scale to the distance
+            cylinderDistance = 0.5f * Vector3.Distance(this.qualitiesPoints[i].transform.position, this.qualitiesPoints[i+1].transform.position);
+            this.ringGameObjects[i].transform.localScale = new Vector3(this.ringGameObjects[i].transform.localScale.x, cylinderDistance, this.ringGameObjects[i].transform.localScale.z);
+
+            // Make the cylinder look at the main point.
+            // Since the cylinder is pointing up(y) and the forward is z, we need to offset by 90 degrees.
+            this.ringGameObjects[i].transform.LookAt(this.qualitiesPoints[i+1].transform, Vector3.up);
+            this.ringGameObjects[i].transform.rotation *= Quaternion.Euler(90, 0, 0);
+        }
+
+        // Draw last line
+
+        // Move the ring to the point
+        this.ringGameObjects[nbQualities - 1].transform.position = this.qualitiesPoints[nbQualities - 1].transform.position;
+
+        // Match the scale to the distance
+        cylinderDistance = 0.5f * Vector3.Distance(this.qualitiesPoints[nbQualities - 1].transform.position, this.qualitiesPoints[0].transform.position);
+        this.ringGameObjects[nbQualities - 1].transform.localScale = new Vector3(this.ringGameObjects[nbQualities - 1].transform.localScale.x, cylinderDistance, this.ringGameObjects[0].transform.localScale.z);
+
+        // Make the cylinder look at the main point.
+        // Since the cylinder is pointing up(y) and the forward is z, we need to offset by 90 degrees.
+        this.ringGameObjects[nbQualities - 1].transform.LookAt(this.qualitiesPoints[0].transform, Vector3.up);
+        this.ringGameObjects[nbQualities - 1].transform.rotation *= Quaternion.Euler(90, 0, 0);
     }
 }
