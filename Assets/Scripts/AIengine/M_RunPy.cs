d0c71eed72441db3b0e06f8d8e22f4255676f8d9
Yasmeen Trifiss <yasmeen.trifiss@gmail.com>
2018-01-26 23:01:58 +0100

    Add RunPy

---
+++ Assets/Scripts/AIengine/M_RunPy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HRAP
+{
+    public class M_RunPy
+    {
+
+        public M_RunPy(string path, string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = path;
+            start.Arguments = string.Format("{0} {1}", cmd, args);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    Console.Write(result);
+                }
+            }
+        }
+    }
+}
---
+++ HRAPTest/RunPyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using HRAP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HRAPTest
+{
+    [TestClass]
+    public class M_RunPyTests
+    {
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+       [TestMethod]
+        public void GetFinalValues()
+        {
+            M_RunPy p = new M_RunPy(@"..\..\..\PythonAssessment\build\AIAssessment win\job_assessment.exe", " ", "");
+        }
+
+    }
+}
