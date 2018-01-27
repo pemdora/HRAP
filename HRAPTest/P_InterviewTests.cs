6692fed84b8923ef0fb1288e419c869fedbd2e37
Yasmeen Trifiss <yasmeen.trifiss@gmail.com>
2018-01-27 10:28:45 +0100

    Add tests for P_Interview

---
+++ HRAPTest/P_InterviewTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using HRAP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HRAPTest
+{
+    [TestClass]
+    public class P_InterviewTests
+    {
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            AIengine.datapath = @"..\..\..\Assets\";
+        }
+
+        [TestMethod]
+        public void LaunchInterview()
+        {
+            // A TESTER SANS IHM
+
+             M_Candidate candidate = new M_Candidate("steve", "chef de projet");
+             P_Interview interview = new P_Interview(candidate);
+             int count = 0;
+             while (!interview.IsOver && count<300)
+             {
+                 interview.Launch();
+                 interview.SetChosenAnswer(1);
+                 count++;
+             }
+
+        }
+    }
+}
