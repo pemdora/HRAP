fdbc9c2304b26b05cb870ae2760791b00f2bb0e3
Yasmeen Trifiss <yasmeen.trifiss@gmail.com>
2018-02-03 13:24:03 +0100

    Clean P_Interview

--- Assets/Scripts/AIengine/P_Interview.cs
+++ Assets/Scripts/AIengine/P_Interview.cs
@@ -11,14 +11,14 @@ namespace HRAP
         private IHMInterview ihm;
         private List<M_Sequence> sequenceList;
         private M_DialogElement previous;
-        private V_Question q;
+        private V_Question q; // current question
         private M_QCM qcm;
         public List<M_Answer> candidateAnswers;
 
 
         private int currentSequence;
         private int currentElement;
-        private bool isWaiting;
+        private bool isWaiting; // is waiting for candidate answer
         private bool isOver;
 
         public P_Interview(M_Candidate candidate, IHMInterview ihm)
@@ -42,20 +42,16 @@ namespace HRAP
 
         public void Launch()
         {
-            // if the candidate has answer 
             if (!isWaiting)
             {
+                // If current dialog element is a question
 
                 if (sequenceList[currentSequence].DialogElements[currentElement] is M_Question)
                 {
+                    // Get question and answers
                     q = GetNextQuestion();
 
-                    Console.WriteLine("Q: " + q.Question);
-                    Console.WriteLine("Num Answers: " + q.NumAnswers);
-                    foreach (string s in q.Answers)
-                    {
-                        Console.WriteLine("A: " + s);
-                    }
+                    // Display question and answers
                     ihm.Activate_buttons_nb_answers(q.NumAnswers);
                     ihm.DisplayQuestion(q.Question);
                     ihm.DisplayAnswers(q.Answers);
@@ -71,15 +67,20 @@ namespace HRAP
                     isWaiting = true;
                 }
 
+                // If current dialog element is a phrase
+
                 if (sequenceList[currentSequence].DialogElements[currentElement] is M_Phrase)
                 {
                     M_Phrase currentPhrase = (M_Phrase)sequenceList[currentSequence].DialogElements[currentElement];
+
+                    // If phrase is first element in sequence, display phrase
                     if (currentElement == 0) currentPhrase.Display = true;
+
+                    // If phrase is next element to previous element, display phrase
                     if (previous != null && currentPhrase.Id == previous.Next) currentPhrase.Display = true;
+
                     if (currentPhrase.Display)
                     {
-
-                        Console.WriteLine("P: " + currentPhrase.Text);
                         ihm.Clear();
                         ihm.DisplayComment(currentPhrase.Text);
                         #region Camera - Animation - Audio 
@@ -97,9 +98,8 @@ namespace HRAP
                 }
 
 
+                // If current element is last element in sequence, set next sequence
 
-
-                // Set next sequence
                 if (currentElement == sequenceList[currentSequence].DialogElements.Count - 1)
                 {
                     if (sequenceList[currentSequence].GetNextSequence() != null)
@@ -110,49 +110,59 @@ namespace HRAP
                     }
                     else
                     {
+                        // there is no more sequence in database
                         this.IsOver = true;
                     }
                 }
 
-                // Set next element
+                // If current element is not last element in sequence, get next element
+
                 if (currentElement < sequenceList[currentSequence].DialogElements.Count - 1)
                 {
-
                     this.currentElement++;
                 }
-
             }
+
             // otherwise do nothing
+
         }
 
-        // Get current dialog element animation
+
+        // Return current dialog element animation
+
         private M_Animation GetCurrentAnimation()
         {
             return sequenceList[currentSequence].DialogElements[currentElement].Animation;
-
         }
 
-        // Get current dialog element camera 
+
+        // Return current dialog element camera 
+
         private M_Camera GetCurrentCamera()
         {
             return sequenceList[currentSequence].DialogElements[currentElement].Camera;
-
         }
 
+
         // Convert next M_Question to V_Question
+
         private V_Question GetNextQuestion()
         {
             V_Question result = null;
 
+            // Get M_Question 
+
             M_Question question = (M_Question)sequenceList[currentSequence].DialogElements[currentElement];
+
+            // If question is found, get answers
 
             if (question != null)
             {
-
                 int count = currentElement + 1;
                 List<M_Answer> answerList = new List<M_Answer>();
                 List<string> answersString = new List<string>();
 
+                // While element is not last element and is an answer, add answer in answerList 
                 while (count < sequenceList[currentSequence].DialogElements.Count
                     && (sequenceList[currentSequence].DialogElements[count] is M_Answer))
                 {
@@ -169,12 +179,11 @@ namespace HRAP
             return result;
         }
 
-        // Set candidate answer in candidateAnswers
+        // Add candidate answer in candidateAnswers
+
         public void SetChosenAnswer(int chosen_answer)
         {
-
-            if (!(sequenceList[currentSequence].DialogElements[currentElement] is M_Phrase)
-                && this.qcm != null)
+            if (!(sequenceList[currentSequence].DialogElements[currentElement] is M_Phrase) && this.qcm != null)
             {
                 M_Answer answerChosen = null;
                 answerChosen = this.qcm.Answers[chosen_answer];
@@ -199,29 +208,19 @@ namespace HRAP
 
 
 
-        // Envoie les competences avec les valeurs du candidat
+        // Return candidate competences at the end of the interview
+
         public List<V_Competence> GetResult1()
         {
-
             List<V_Competence> result = new List<V_Competence>();
 
-            // Récupère les valeurs des qualités des réponses
+            // Update the values of answers qualities
             candidateAnswers = M_DataManager.Instance.UpdateQualityPoints(candidateAnswers);
 
-            /*
-            foreach (M_Answer a in candidateAnswers)
-            {
-                foreach (M_Quality q in a.QualitiesList)
-                {
-                    AIengine.Affiche(q.Points.ToString());
-                }
-                AIengine.Affiche("*************");
-            }*/
-
-            // Transforme les qualités en compétences
+            // Convert qualities to competences
             candidate.UpdateCompetences(candidateAnswers);
 
-            // Envoie le résultat sous forme de dictionnaire
+            // Add competences in result
             foreach (M_Competence c in candidate.CompetencesList)
             {
                 result.Add(new V_Competence(c.Name, c.Points));
